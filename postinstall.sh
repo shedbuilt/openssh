94f76ef557eebff427b7d4e8b55d414364b94fd0
Auston Stewart <auston@shedbuilt.net>
2018-02-07 10:57:08 -1000

    Updating to 7.6p1

--- postinstall.sh
+++ postinstall.sh
@@ -3,11 +3,11 @@ if [ ! -e /etc/ssh/ssh_config ]; then
     install -m644 /etc/ssh/ssh_config.default /etc/ssh/ssh_config
 fi
 if [ ! -e /etc/ssh/sshd_config ]; then
-    install -m644 /etc/ssh/sshd_config.default /etc/ssh/sshd_config        
+    install -m644 /etc/ssh/sshd_config.default /etc/ssh/sshd_config
 fi
 getent passwd sshd
-if [ ! $? -eq 0 ]; then
-    install -v -m700 -d /var/lib/sshd
+if [ $? -ne 0 ]; then
+    install -vdm700 /var/lib/sshd
     chown -v root:sys /var/lib/sshd
     groupadd -g 50 sshd
     useradd -c 'sshd PrivSep' \
