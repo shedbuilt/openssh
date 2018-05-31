9f041e96f50d0caf4c434e90b8bf64ce65c0b8bc
Auston Stewart <auston@shedbuilt.net>
2018-05-31 10:58:17 -1000

    Updating to v7.7p1 for blank

--- build.sh
+++ build.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/openssh-7.6p1-openssl-1.1.0-1.patch" && \
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Patch for OpenSSL 1.1 support
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/openssh-7.7p1-openssl-1.1.0-1.patch" &&
+# Configure
 ./configure --prefix=/usr                     \
             --sysconfdir=/etc/ssh             \
             --with-md5-passwords              \
-            --with-privsep-path=/var/lib/sshd && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-mkdir -pv "${SHED_FAKE_ROOT}/usr/bin"
-install -v -m755 contrib/ssh-copy-id "${SHED_FAKE_ROOT}/usr/bin"
-install -dm755 "${SHED_FAKE_ROOT}/lib/systemd/system"
-install -m644 "${SHED_PKG_CONTRIB_DIR}/sshd.service" "${SHED_FAKE_ROOT}/lib/systemd/system/"
-install -m644 "${SHED_PKG_CONTRIB_DIR}/sshdat.service" "${SHED_FAKE_ROOT}/lib/systemd/system/sshd@.service"
-install -m644 "${SHED_PKG_CONTRIB_DIR}/sshd.socket" "${SHED_FAKE_ROOT}/lib/systemd/system/"
-mkdir -pv "${SHED_FAKE_ROOT}/usr/share/man/man1"
-install -v -m644 contrib/ssh-copy-id.1 "${SHED_FAKE_ROOT}/usr/share/man/man1"
-install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/doc/openssh-7.6p1"
-install -v -m644 INSTALL LICENCE OVERVIEW README* "${SHED_FAKE_ROOT}/usr/share/doc/openssh-7.6p1"
-mv "${SHED_FAKE_ROOT}/etc/ssh/ssh_config" "${SHED_FAKE_ROOT}/etc/ssh/ssh_config.default"
-mv "${SHED_FAKE_ROOT}/etc/ssh/sshd_config" "${SHED_FAKE_ROOT}/etc/ssh/sshd_config.default"
-echo "PermitRootLogin yes" >> "${SHED_FAKE_ROOT}/etc/ssh/sshd_config.default"
+            --with-privsep-path=/var/lib/sshd &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange
+mkdir -pv "${SHED_FAKE_ROOT}/usr/bin" &&
+install -v -m755 contrib/ssh-copy-id "${SHED_FAKE_ROOT}/usr/bin" &&
+# Install systemd units
+install -vdm755 "${SHED_FAKE_ROOT}/lib/systemd/system" &&
+install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshd.service" "${SHED_FAKE_ROOT}/lib/systemd/system/" &&
+install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshdat.service" "${SHED_FAKE_ROOT}/lib/systemd/system/sshd@.service" &&
+install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshd.socket" "${SHED_FAKE_ROOT}/lib/systemd/system/" &&
+mkdir -pv "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
+install -v -m644 contrib/ssh-copy-id.1 "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
+# Install Default Config Files
+echo "PermitRootLogin yes" >> "${SHED_FAKE_ROOT}/etc/ssh/sshd_config" &&
+install -vdm755 "${SHED_FAKE_ROOT}/usr/share/defaults/etc/ssh" &&
+mv "${SHED_FAKE_ROOT}/etc/ssh/ssh_config" "${SHED_FAKE_ROOT}/usr/share/defaults/etc/ssh" &&
+mv "${SHED_FAKE_ROOT}/etc/ssh/sshd_config" "${SHED_FAKE_ROOT}/usr/share/defaults/etc/ssh" || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
+    install -v -m644 INSTALL LICENCE OVERVIEW README* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
--- postinstall.sh
+++ postinstall.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 if [ ! -e /etc/ssh/ssh_config ]; then
-    install -m644 /etc/ssh/ssh_config.default /etc/ssh/ssh_config
+    ln -sfv /usr/share/defaults/etc/ssh/ssh_config /etc/ssh/ssh_config
 fi
 if [ ! -e /etc/ssh/sshd_config ]; then
-    install -m644 /etc/ssh/sshd_config.default /etc/ssh/sshd_config
+    ln -sfv /usr/share/defaults/etc/ssh/sshd_config /etc/ssh/sshd_config
 fi
 getent passwd sshd
 if [ $? -ne 0 ]; then
-    install -vdm700 /var/lib/sshd
-    chown -v root:sys /var/lib/sshd
-    groupadd -g 50 sshd
+    install -vdm700 /var/lib/sshd &&
+    chown -v root:sys /var/lib/sshd &&
+    groupadd -g 50 sshd &&
     useradd -c 'sshd PrivSep' \
             -d /var/lib/sshd \
             -g sshd \
             -s /bin/false \
-            -u 50 sshd
-    ssh-keygen -A
+            -u 50 sshd &&
+    ssh-keygen -A &&
     systemctl enable sshd.service
 fi
