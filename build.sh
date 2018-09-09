f513fffe5e1d773956c70b25930b2c3daec64991
Auston Stewart <auston@shedbuilt.net>
2018-09-09 11:38:57 -1000

    Updating to 7.8p1

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Patch for OpenSSL 1.1 support
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/openssh-7.7p1-openssl-1.1.0-1.patch" &&
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/openssh-7.8p1-openssl-1.1.0-1.patch" &&
 # Configure
 ./configure --prefix=/usr                     \
             --sysconfdir=/etc/ssh             \
@@ -14,13 +13,13 @@ make DESTDIR="$SHED_FAKE_ROOT" install &&
 # Rearrange
 mkdir -pv "${SHED_FAKE_ROOT}/usr/bin" &&
 install -v -m755 contrib/ssh-copy-id "${SHED_FAKE_ROOT}/usr/bin" &&
+mkdir -pv "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
+install -v -m644 contrib/ssh-copy-id.1 "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
 # Install systemd units
 install -vdm755 "${SHED_FAKE_ROOT}/lib/systemd/system" &&
 install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshd.service" "${SHED_FAKE_ROOT}/lib/systemd/system/" &&
 install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshdat.service" "${SHED_FAKE_ROOT}/lib/systemd/system/sshd@.service" &&
 install -vm644 "${SHED_PKG_CONTRIB_DIR}/sshd.socket" "${SHED_FAKE_ROOT}/lib/systemd/system/" &&
-mkdir -pv "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
-install -v -m644 contrib/ssh-copy-id.1 "${SHED_FAKE_ROOT}/usr/share/man/man1" &&
 # Install Default Config Files
 echo "PermitRootLogin yes" >> "${SHED_FAKE_ROOT}/etc/ssh/sshd_config" &&
 install -vdm755 "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_INSTALL_DIR}/etc/ssh" &&
@@ -28,6 +27,6 @@ mv "${SHED_FAKE_ROOT}/etc/ssh/ssh_config" "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_
 mv "${SHED_FAKE_ROOT}/etc/ssh/sshd_config" "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_INSTALL_DIR}/etc/ssh" || exit 1
 # Install Documentation
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
-    install -v -m644 INSTALL LICENCE OVERVIEW README* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}" &&
+    install -v -m644 INSTALL LICENCE OVERVIEW README* "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 fi
